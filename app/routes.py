0633b8fca99faf84ec2a01c0ec09ddd8998f4f05
“OladayoOyedeji” <robertoyedeji7gmail.com>
2025-03-03 17:17:35 -0800

    third commit

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from app import app
+from flask import request
 
+password_entered = False
 password_valid = True
+valid_length = 8
+no_capital_letters = 1
+no_integers = 2
+no_special_char = 2
 
 @app.route('/')
 def homepage():
@@ -11,62 +17,116 @@ def homepage():
     </a>
 </html>'''
 
+def valid_check(Password):
+    global password_valid
+    global valid_length
+    global no_capital_letters
+    global no_integers
+    global no_special_char
+    global password_entered
+    
+    # print("password_entered", )
+    if not password_entered:
+        return False
+    
+    password_valid = False
+    
+    if len(Password) < valid_length:
+        return False;
+    count_cl = 0
+    count_int = 0
+    count_sp_c = 0
+    for c in Password:
+        if ord(c) >= ord('A') and ord(c) <= ord('Z'):
+            count_cl += 1
+        elif ord(c) >= ord('0') and ord(c) <= ord('9'):
+            count_int += 1
+        elif ord(c) < ord('a') or ord(c) > ord('z'):
+            count_sp_c += 1
+    valid = count_cl >= no_capital_letters and \
+    count_int >= no_integers and count_sp_c >= no_special_char
+
+    print("password_valid?:", valid)
+    password_valid = valid
+        
+    return valid
+
 def valid():
+    global password_entered
     Password = request.args.get('password', '[no password]')
-    if Password is valid:
-        return 'add_b'
+    if valid_check(Password):
+        print("password valid")
+        return 'add_db'
     else:
+        print('true')
+        password_entered = True
         return 'register'
 
+def ver_message():
+    global password_valid
+    global valid_length
+    global no_capital_letters
+    global no_integers
+    global no_special_char
+    if password_valid:
+        return ''
+    else:
+        return '<span style = "color: red; font-size: 10px">\
+        password has to be a minimum of %(i)s characters \
+        containing %(j)s capital letters, %(k)s integers, %(l)s \
+        special characters<\span><br>\n <span style = "color: black">'\
+        %{'i':valid_length, 'j':no_capital_letters,
+                              'k':no_integers, 'l':no_special_char}
+    
 @app.route('/register')
 def register():
     return """"<html>
 <body>
 <h1>Registration</h1>
-<form action='%s' method='get'>
-Lastname: <input type='text' name='lastname'/> 
+<form action='%(action)s' method='get'><br>
+Lastname: <input type='text' name='lastname'/> <br>
 Firstname: <input type='text' name='firstname'/> <br>
 Password: <input type='text' name='password'/> <br>
-   
-Email: <input type='text' name='email'/> <br>
+%(verified_message)s
+<h3>Email: <input type='text' name='email'/></h3> <br>
 <input type='submit' value='Go'/>
 </form>
 </body>
-</html>""" % valid()
+</html>""" % {'action':valid(), 'verified_message':ver_message()}
 
 
 
-def process_email(Email):
+# def process_email(Email):
     
 
 
-def add_db():
-    lastname = request.args.get("lastname", "[No Name]")
-    firstname = request.args.get("fullname", "[No Name]")
-    Password = request.args.get('password', '[no password]')
-    Email = request.args.get('email', 'no email')
+# def add_db():
+#     lastname = request.args.get("lastname", "[No Name]")
+#     firstname = request.args.get("fullname", "[No Name]")
+#     Password = request.args.get('password', '[no password]')
+#     Email = request.args.get('email', 'no email')
 
-    try:
-        process_email(Email)
-        Hash = hash_password(Password)
-    except Exception as e:
-        return False
+#     try:
+#         process_email(Email)
+#         Hash = hash_password(Password)
+#     except Exception as e:
+#         return False
     
-    conn = pymsql.connect(user='root', passwrd='root', db='talbook')
-    cursor = conn.cursor(pymysql.cursor.DictCursor)
+#     conn = pymsql.connect(user='root', passwrd='root', db='talbook')
+#     cursor = conn.cursor(pymysql.cursor.DictCursor)
 
     
-    exe = 'insert into Person lastname=%(l)s, firstname=%(f)s, hash=%(h)s, email=%(e)s';
-    cursor.execute(exe)
+#     exe = 'insert into Person lastname=%(l)s, firstname=%(f)s, hash=%(h)s, email=%(e)s';
+#     cursor.execute(exe)
     
-    cursor.commit()
-    # try:
-    #     table = cursor.fetchall()
-    #     row = table[len(table)-1]
-    #     person_id = row['person_id']
+#     cursor.commit()
+#     # try:
+#     #     table = cursor.fetchall()
+#     #     row = table[len(table)-1]
+#     #     person_id = row['person_id']
 
-    #     #now i want to add the Email
-    #     exe = 'insert into Pa lastname=%(l)s, firstname=%(f)s';
+#     #     #now i want to add the Email
+#     #     exe = 'insert into Pa lastname=%(l)s, firstname=%(f)s';
 
-@app.route('/')
-def
+# @app.route('/')
+# def
