cb458aa6a9fe4e631323029704bc7ee871699cfc
ctschwandt <bigbrovc@gmail.com>
2025-04-05 14:13:17 -0700

    sync

--- app/db/User.sql
+++ app/db/User.sql
@@ -1,4 +1,4 @@
 USE Talbook;
-INSERT INTO User (email, username, password_hash) VALUES ('bigbrovc@gmail.com', 'bigbrovc', '$2b$11$ftRmga.BcZr7Xe6FSlxVIuuBJtdfvu937k1Xh2jM204irzC78Z2Mq');
-INSERT INTO User (email, username, password_hash) VALUES ('oroyedeji1@cougars.ccis.edu', 'Robiefresh', '$2b$11$zga4ZOC5znkbDOufgQ68ouRhN3ok65CKoJFRtKv0nBzITw84xkTK.');
-INSERT INTO User (email, username, password_hash) VALUES ('nalahaideb@cougars.ccis.edu', 'nashydog', '$2b$11$.0R1dyCg18jdzhzYgsRbAefZVdj/buL1x7GHt.WszYzZcY7r9Xwiu');
+INSERT INTO User (email, username, password_hash) VALUES ('bigbrovc@gmail.com', 'bigbrovc', '$2b$11$.eTJkU0JhvIvPC15KeN1pe8qAw53WQD3Q0tn01Tq1Pshnxucm1XgS');
+INSERT INTO User (email, username, password_hash) VALUES ('oroyedeji1@cougars.ccis.edu', 'Robiefresh', '$2b$11$Da8XlMMSpT7kHlMn4NxAxO1np6TTdHWwDpQV9BI5oZCUC8F959zdy');
+INSERT INTO User (email, username, password_hash) VALUES ('nalahaideb@cougars.ccis.edu', 'nashydog', '$2b$11$I3BVimq6wUMVigogdn1XyOAkHoF8Slv6Fs9UMlCOKihHN5.gloYOW');
