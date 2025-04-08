2fa172224ba0c606634109ce38be2893c2002b33
ctschwandt <bigbrovc@gmail.com>
2025-04-08 09:13:16 -0700

    sync

--- app/handlers/selling.py
+++ app/handlers/selling.py
@@ -1,5 +1,14 @@
 # File: selling.py
 from flask import request, render_template, flash, redirect, url_for
+from app.utils import mysql_util
 
 def handle_selling():
-    return render_template('selling.html')
+    sql = '''
+    SELECT T.name, COUNT(IT.item_id) AS tag_count
+    FROM Tag as T
+    JOIN Item_Tag AS IT ON T.id=IT.tag_id
+    GROUP BY T.name;
+    '''
+    tag_count = mysql_util.execute_sql(sql, fetchdict=True)
+    tag_count = {item['name']: item['tag_count'] for item in tag_count}
+    return render_template('selling.html', tag_count=tag_count)
