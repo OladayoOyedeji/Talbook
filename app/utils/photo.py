2b2eb080174bf7eea18f28ffc15af2b41142acfd
ctschwandt <bigbrovc@gmail.com>
2025-04-09 21:32:21 -0700

    sync

--- app/utils/photo.py
+++ app/utils/photo.py
@@ -7,10 +7,10 @@ import os
 from werkzeug.utils import secure_filename
 
 from app.utils import mysql_util
-# from app import app
+from app import app
 
 UPLOAD_FOLDER = 'app/static/uploads/'
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
@@ -21,11 +21,13 @@ def allowed_file(filename):
 def get_image(request):
     if 'file' not in request.files:
         flash('No file part')
+        print('No file part')
         return None
     
     file = request.files['file']
     if file.filename == '':
         flash('No image selected for uploading')
+        print('No image selected for uploading')
         return None
     
     if file and allowed_file(file.filename):
@@ -33,9 +35,11 @@ def get_image(request):
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         #print('upload_image filename: ' + filename)
         flash('Image successfully uploaded and displayed below')
-        return filename
+        print((os.path.join(app.config['UPLOAD_FOLDER'], filename)))
+        return (os.path.join(app.config['UPLOAD_FOLDER'], filename))
     else:
         flash('Allowed image types are - png, jpg, jpeg, gif')
+        print("here?")
         return None
 
 def upload_image(file: str):
@@ -86,7 +90,7 @@ def update_User_photo_id(photo_id, user_id):
     sql = '''
     UPDATE User SET photo_id = %s WHERE user_id = %s
     '''
-    mysql_util.execute(sql, (photo_id, user_id))
+    mysql_util.execute(sql, (photo_id, user_id), True)
 
 if __name__ == '__main__':
     upload_image('app/static/images/uploads/harp.webp')
