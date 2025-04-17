2280cd3153f49ad2e17a4b97bf9ac510365ba4ae
ctschwandt <167057535+ctschwandt@users.noreply.github.com>
2025-04-17 12:06:50 -0500

    Delete app/db/User.sql

--- app/db/User.sql
+++
@@ -1,7 +0,0 @@
-USE Talbook;
-
-INSERT INTO User (email, username, password_hash) VALUES ('bigbrovc@gmail.com', 'bigbrovc', '$2b$11$3ICXa7dHM/ZAcyWQn2LvCuHVx4OK1lzQpToC2S5rahVaQWK64giXm');
-INSERT INTO User (email, username, password_hash) VALUES ('oroyedeji1@cougars.ccis.edu', 'Robiefresh', '$2b$11$qGDJL2uL0QpFML4peBcWWuXlUXOEEb2EmiG6kIL3C/FCzpNrZM3rK');
-INSERT INTO User (email, username, password_hash) VALUES ('nalahaideb@cougars.ccis.edu', 'nashydog', '$2b$11$H8P8VvWJlfUy6OmBxxivCuVQAxoXp/D/N6PZGQiOHxTYWs13e6Foa');
-INSERT INTO User (email, username, password_hash) VALUES ('barack_obama@whitehouse.gov', 'barack_obama', '$2b$11$TBcg5toJSXrXJoRKtnYjwOt4b0jqKf79amzpFHeqUJHpC0PJBKMS.');
-INSERT INTO User (email, username, password_hash) VALUES ('emutlu1@cougars.ccis.edu', 'elif_cato', '$2b$11$34E1votWnfB5h9pK9XlFZeENYmzAYHpBnVYYWevwaCThzT3aWvmxO');
