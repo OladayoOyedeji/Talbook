b78413f4e552de8a1b0be0db118fcd663de61404
ctschwandt <bigbrovc@gmail.com>
2025-04-19 11:21:48 -0700

    sync

--- app/db/User.sql
+++ app/db/User.sql
@@ -1,6 +1,6 @@
 USE Talbook;
-INSERT INTO User (email, username, password_hash) VALUES ('bigbrovc@gmail.com', 'bigbrovc', '$2b$11$D7ZZKr8.tbOqwxPxvV2cP.0b9RW1z9C.wE5IADbGMgeQOv1OvN0am');
-INSERT INTO User (email, username, password_hash) VALUES ('oroyedeji1@cougars.ccis.edu', 'Robiefresh', '$2b$11$DnSV/JlxctpGvSY1UWjVYehUmd6E2J51yXEtRcEvvoSoxJWsV0l5G');
-INSERT INTO User (email, username, password_hash) VALUES ('nalahaideb@cougars.ccis.edu', 'nashydog', '$2b$11$9VkuIBMnVkBS/sAvQljhJ.b3kbb5L7CAfY4kY6CPTykqNKSTo5b1K');
-INSERT INTO User (email, username, password_hash) VALUES ('barack_obama@whitehouse.gov', 'barack_obama', '$2b$11$hsvfU/wnuf1U4q6xnmO6gegQZBSdAevab6yDT1UBQGWCvgjbbyh72');
-INSERT INTO User (email, username, password_hash) VALUES ('emutlu1@cougars.ccis.edu', 'elif_cato', '$2b$11$3YVQjsh0GafSyl0EegSAu.cPmtXYWupWqHh.emDHIScSxbKolvnWq');
+INSERT INTO User (email, username, password_hash) VALUES ('bigbrovc@gmail.com', 'bigbrovc', '$2b$11$GiJJ82QwMkYtDO4n8Ek2beUTK01M543moOyKmy7K2B..GhwFNkipy');
+INSERT INTO User (email, username, password_hash) VALUES ('oroyedeji1@cougars.ccis.edu', 'Robiefresh', '$2b$11$3IrZKA7ML1exGSMpfvlmI.9dC2GBX64QaRd8SPpuZuxYgA/CDqJoC');
+INSERT INTO User (email, username, password_hash) VALUES ('nalahaideb@cougars.ccis.edu', 'nashydog', '$2b$11$VtNqr1lf1edOsYaWkwSMTOFZkWvRdk0ry/NZXiddlPxZTVRY3h.IC');
+INSERT INTO User (email, username, password_hash) VALUES ('barack_obama@whitehouse.gov', 'barack_obama', '$2b$11$SC6F8xTMkrzmmCBetFD3Zuccm/7ID6FLqgwxM96vcQ.kbFmvml6/e');
+INSERT INTO User (email, username, password_hash) VALUES ('emutlu1@cougars.ccis.edu', 'elif_cato', '$2b$11$ELehEdRs/56eyoP9NM3k/.m5tBr/kE7L9IebCbj4m/rmK1TX98T3W');
