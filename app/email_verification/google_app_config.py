82bb8f1f0c59114d4c7de27fd8e2bfa79538eac0
ctschwandt <167057535+ctschwandt@users.noreply.github.com>
2025-04-28 09:28:06 -0500

    Update google_app_config.py

--- app/email_verification/google_app_config.py
+++ app/email_verification/google_app_config.py
@@ -1,3 +1,3 @@
 GMAIL = 'oyedejirobert0164882@gmail.com' # your gmail
-GOOGLE_APP_PASSWORD = 'enuzdrrykkqvygpf' # google app password of length 16 (see below)
+GOOGLE_APP_PASSWORD = # secret
 TO = 'oroyedeji1@cougars.ccis.edu'
